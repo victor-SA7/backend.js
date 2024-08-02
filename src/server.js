1aca4407cbac0d1e65c378c4aacaaf788a0d5cce
Kelvys Moura <kelvysmoura2@gmail.com>
2024-08-02 11:59:33 -0300

    Iniciando em express api com isomnia

--- src/server.js
+++ src/server.js
@@ -1,3 +1,42 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const ProductModel = require('./models/ProductModel');
+const UserModel = require('./models/UserModel');
+
+app.get('/products', async function(request, response) {
+    const products = await ProductModel.findAll();
+    response.json(products);
+});
+
+app.get('/users', async function(request, response) {
+    const users = await UserModel.findAll();
+    response.json(users);
+});
+
+app.post('/users', function(resquest, response) {
+    UserModel.create(resquest.body);
+    return response.json({
+        message: "Usuario criado com sucesso!"
+    });
+});
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const UserController = require('./controllers/UserController');
 
 // const request = {
@@ -14,15 +53,15 @@
 
 
 
-const ProductController = require('./controllers/ProductController');
+// const ProductController = require('./controllers/ProductController');
 
-const request = {
-    body: {
-        name: "Motorola",
-        price: 19.58,
-        description: "Descrição do produto"
-    }
-}
+// const request = {
+//     body: {
+//         name: "Motorola",
+//         price: 19.58,
+//         description: "Descrição do produto"
+//     }
+// }
 
-// ProductController.create(request);
-ProductController.list();
+// // ProductController.create(request);
+// ProductController.list();
